50bffe6b419423f26e6c633a08559ce547e4891a
Parviz Haydarov <parvizi2003@gmail.com>
2024-01-28 22:37:20 +0500

    filters added

--- routes.js
+++ routes.js
@@ -5,8 +5,30 @@ const router = new Router();
 
 router.get('/', async (req, res) => {
     if (req.session.userID) {
-        let table = (await db.query(`SELECT job_name, done_date, time_to_finish FROM jobs WHERE user_id = ${req.session.userID}`)).rows;
+        if(req.session.username == 'admin') {
+            let table = (await db.query(`
+            SELECT user_name, job_name, done_date, time_to_finish 
+            FROM jobs
+            JOIN users USING (user_id)
+            WHERE user_name = $1
+            ORDER BY ${req.session.order}
+            `, [req.session.orderByUser])).rows;
+            let users = (await db.query('SELECT user_name FROM users')).rows
+            res.render('index', {
+                title: 'Jobs-Done',
+                username: req.session.username,
+                table: table,
+                show: true,
+                users: users
+            })
+            return
+        } 
+        console.log(req.session.order)
+        let table = (await db.query(`SELECT job_name, done_date, time_to_finish 
+            FROM jobs WHERE user_id = $1 
+            ORDER BY ${req.session.order}`, [req.session.userID])).rows;
         res.render('index', {
+            title: 'Jobs-Done',
             username: req.session.username,
             table: table
         })
@@ -24,14 +46,16 @@ router.post('/', async (req, res) => {
 })
 
 router.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login', {title: 'Login'})
 });
 
 router.post('/login', async (req, res) => {
     let user = (await db.query('SELECT * FROM users WHERE user_name = $1 AND user_password = $2', [req.body.username, req.body.password])).rows[0];
     if (user) {
         req.session.userID = user.user_id;
-        req.session.username = user.user_name
+        req.session.username = user.user_name;
+        req.session.orderByUser = user.user_name;
+        req.session.order = 'job_name';
         res.redirect('/');
     } else {
         res.render('login', {show: true});   
@@ -39,7 +63,7 @@ router.post('/login', async (req, res) => {
 });
 
 router.get('/registration', async (req, res) => {
-    res.render('registration')
+    res.render('registration', {title: 'Registration'})
 })
 
 router.post('/registration', async (req, res) => {
@@ -63,7 +87,7 @@ router.get('/logout', (req, res) => {
 
 router.get('/changePassword', (req, res) => {
     if (req.session.userID) {
-        res.render('changePassword');    
+        res.render('changePassword', {title: 'Change Password'});    
     } else {
         res.redirect('/login');
     }
@@ -79,6 +103,16 @@ router.post('/changePassword', async (req, res) => {
     } else {
         res.render('changePassword', {show: true});    
     }
+})
+
+router.get('/order-by/:order', (req, res) => {
+    req.session.order = req.params.order;
+    res.redirect('/')
+})
+
+router.get('/user/:user', (req, res) => {
+    req.session.orderByUser = req.params.user;
+    res.redirect('/')
 })
 
 async function isTaken(username) {
